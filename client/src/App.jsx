c71283ba3221be28ed1077c00b47f3b86751ac2e
Madison <madie-v@live.com>
2024-02-22 20:37:13 -0600

    tyler help

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css';
-import Hero from './components/Hero/Hero';
+import {Outlet} from 'react-router-dom';
 function App() {
  return (
     <div className="App">
-        <Hero/>
+        <Outlet />
     </div>
     );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import App from './App.jsx';
+import Home from  './pages/Home.jsx';
+//import Login from './pages/Login.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: < ErrorPage />, 
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            }
+        ]
+    }
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
