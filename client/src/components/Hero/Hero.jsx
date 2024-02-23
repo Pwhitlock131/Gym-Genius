3a551adcefda08ceb15baac1589979ea9fcb2dc3
unknown <el_word9@yahoo.com>
2024-02-22 19:17:12 -0600

    Added basic starting code/fixed some code

---
+++ client/src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Hero.css'
+const Hero = () => {
+  return (
+    <div classname="hero">
+    <div className="left-hero">left side</div>
+    <div className="right-hero">right side</div>
+      
+    </div>
+  )
+}
+
+export default Hero
