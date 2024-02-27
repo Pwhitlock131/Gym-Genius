9c1a3d84b6fe2f4f774ff226336d6d18113a2529
Pwhitlock <pwhitlock131@gmail.com>
2024-02-26 21:18:58 -0600

    Please

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.css'; // Import the CSS file
-import '../components/Posts/Calender.jsx';
+import '../../src/components/Posts/Calender.jsx';
 import '../../src/components/Navbar/Navbar'
 
 
