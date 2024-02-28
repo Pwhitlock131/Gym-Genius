6d5fda745d93a5d895f92421287d45a83ba91f7b
Laulecmor <lauren.morrison00@gmail.com>
2024-02-27 20:59:30 -0600

    from madison's slack

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import './Home.css'; // Import the CSS file
-import '../../src/components/Navbar/Navbar'
-
-
-
+// import React from 'react';
+// import './Home.css'; // Import the CSS file
+// const Home = () => {
+//   return (
+//     <div className="home-container">
+//       <h1 className="home-title">Welcome to Your Personal Gym Genius</h1>
+//       <p className="home-description">
+//         This is the home page of our fitness Gym Genius application.
+//       </p>
+//       {/* Add more info if necessary */}
+//     </div>
+//   );
+// };
+// export default Home;
+import React from "react";
+// import { Navbar, Nav } from "react-bootstrap";
+// import { Link, useLocation } from "react-router-dom";
+// import Auth from "../utils/auth"
+// import heart from "/images/stethheart.png"
+// import Header from "./Header.jsx"
+import '../../src/components/Navbar/Navbar.jsx'
+import '../../src/components/Posts/Calender.jsx';
 export default function Home() {
-
   // const loggedIn = Auth.loggedIn();
   // const location = useLocation();
   // const isHomePage = location.pathname === '/';
   // const isLoginPage = location.pathname === '/login';
   // const isSignupPage = location.pathname === '/signup';
-
-
-
-
   return (
-
     <>
     <h1>MY HOMEPAGE</h1>
     </>
